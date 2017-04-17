9bf514a8ce03727df62a0800de4230474131bc46
xeon zolt <xeon zolt>
2017-04-17 09:21:59 +0530

    added question in comments of libs
    and we should mouve regex def to the lib or a new file so it should look
    little clean

--- lib/speedtest.py
+++ lib/speedtest.py
@@ -3,3 +3,9 @@ import os
 import sys
 os.system("speedtest")
 
+#usage
+'''
+what is my net|internet speed
+speed of net|internet
+my current net|internet speed
+'''
--- lib/time.py
+++ lib/time.py
@@ -13,3 +13,15 @@ def calander():
     os.system("calander")
 def time_and_date():
     os.system("date +%Y-%m-%d Time : %l:%M:%S ")
+
+    
+#usage
+'''
+what is time
+what is date
+what is full time
+show me date and time
+show me calander 
+show me date
+show me time
+'''
